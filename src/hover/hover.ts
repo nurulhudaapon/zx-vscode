a1d6f11ce5633a8ff20affb993196f71fc7bc139
Araf Ahmed <nayemhasan1314@gmail.com>
2025-11-14 22:43:17 +0600

    update: make autocomplete feature - when user type < it show html suggestion , when hit enter its close automatically

---
+++ src/hover/hover.ts
@@ -0,0 +1,13 @@
+// import { getLanguageService } from "vscode-html-languageservice";
+
+// async provideHover(uri, position, token, next){
+//     const languageService = getLanguageService();
+//     const document = await workspace.openTextDocument(uri);
+//     const htmlDocument = languageService.parseHTMLDocument(document);
+//     const hover = languageService.doHover(
+//       document,
+//       position,
+//       htmlDocument,
+//     );
+//     console.log(hover);
+// }
